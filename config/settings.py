2e2f9a6c5969b1087d5e96d7052161195c758d59
ZongyaoYang <zongyaoyang@gmail.com>
2026-09-15 17:41:49 -0600

    register add and DRF

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # DRF
+    'rest_framework',
+    # my app
+    'books'
 ]
 
 MIDDLEWARE = [
