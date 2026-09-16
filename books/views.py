3dd5b01709584f52fd1fe44cc096078b619d0aed
ZongyaoYang <zongyaoyang@gmail.com>
2026-09-16 11:08:41 -0600

    add custom action to ViewSet

--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Book
 from .serializers import BookSerializer
@@ -8,3 +10,11 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    @action(detail=True, methods=['post'])
+    def mark_unavailable(self, request, pk=None):
+        book = self.get_object()
+        book.is_available = False
+        book.save()
+        serializer = self.get_serializer(book)
+        return Response(serializer.data, status=status.HTTP_200_OK)
