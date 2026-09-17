414eb2baca31d194319b24cf67fcc7168dc4daf9
ZongyaoYang <zongyaoyang@gmail.com>
2026-09-16 21:51:20 -0600

    add author serializer and nested it to bookSerializer

--- books/serializers.py
+++ books/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
-from .models import Book
+from .models import Author, Book
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ["id", "name", "birth_date", "bio"]
 
 
 class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+    author_id = serializers.PrimaryKeyRelatedField(
+        queryset=Author.objects.all(), source="author", write_only=True
+    )
+
     class Meta:
         model = Book
         fields = [  # noqa: RUF012
