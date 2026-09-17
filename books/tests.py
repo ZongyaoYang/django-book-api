bdf20775127d4ae7a64b123c13ce17a018eb5f0f
ZongyaoYang <zongyaoyang@gmail.com>
2026-09-17 11:56:04 -0600

    add APITestCase and implement test_list_books

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from .models import Author, Book
 
 # Create your tests here.
+
+
+class BookAPITest(APITestCase):
+    def setUp(self):
+        self.author = Author.objects.create(name="Frank Herbert")
+        self.book = Book.objects.create(
+            title="Dune",
+            author=self.author,
+            isbn="9780441172719",
+            pages=412,
+            published_date="1965-08-01",
+        )
+        
+    def test_list_books(self):
+        response = self.client.get("/api/books/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
