08e8726656dd3f2cd1262191e8ba6017e083144d
ZongyaoYang <zongyaoyang@gmail.com>
2026-09-16 19:29:00 -0600

    create author model and run migrate

--- books/models.py
+++ books/models.py
@@ -2,9 +2,21 @@ from django.db import models
 
 
 # Create your models here.
+class Author (models.Model):
+    name = models.CharField(max_length=200)
+    birth_date=models.DateField(null=True, blank=True)
+    bio = models.TextField(blank=True)
+    
+    def __str__(self) -> str:
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name="books"
+    )
     isbn = models.CharField(max_length=13, unique=True)
     pages = models.PositiveIntegerField()
     published_date = models.DateField()
@@ -13,5 +25,4 @@ class Book(models.Model):
     
     def __str__(self):
         return f"{self.title} by {self.author}"
-    
     
